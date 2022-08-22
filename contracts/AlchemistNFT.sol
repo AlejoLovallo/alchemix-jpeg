5660623de98ce350bf091d0aefabdcf54a0313d4
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-08-21 22:36:13 -0300

    feat(wip): contract creation

---
+++ contracts/AlchemistNFT.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.8.11;
+
+/// @title  AlchemistNFT
+/// @author Alchemix Finance
+contract AlchemistNFT{
+    /// @dev Checks the whitelist for msg.sender.
+    ///
+    /// Reverts if msg.sender is not in the whitelist.
+    function _onlyWhitelisted() internal view {
+        // Check if the message sender is an EOA. In the future, this potentially may break. It is important that functions
+        // which rely on the whitelist not be explicitly vulnerable in the situation where this no longer holds true.
+        if (tx.origin == msg.sender) {
+          return;
+        }
+
+        // Only check the whitelist for calls from contracts.
+        if (!IWhitelist(whitelist).isWhitelisted(msg.sender)) {
+          revert Unauthorized();
+        }
+    }
+
+    /// @dev Checks an expression and reverts with an {IllegalArgument} error if the expression is {false}.
+    ///
+    /// @param expression The expression to check.
+    function _checkArgument(bool expression) internal pure {
+        if (!expression) {
+            revert IllegalArgument();
+        }
+    }
+
+    /// @dev Checks an expression and reverts with an {IllegalState} error if the expression is {false}.
+    ///
+    /// @param expression The expression to check.
+    function _checkState(bool expression) internal pure {
+        if (!expression) {
+            revert IllegalState();
+        }
+    }
+
+    /// @dev Adds two unsigned 256 bit integers together and returns the result.
+    ///
+    /// @dev This operation is checked and will fail if the result overflows.
+    ///
+    /// @param x The first operand.
+    /// @param y The second operand.
+    ///
+    /// @return z The result.
+    function _uadd(uint256 x, uint256 y) internal pure returns (uint256 z) { z = x + y; }
+
+    /// @dev Subtracts two unsigned 256 bit integers together and returns the result.
+    ///
+    /// @dev This operation is checked and will fail if the result overflows.
+    ///
+    /// @param x The first operand.
+    /// @param y The second operand.
+    ///
+    /// @return z the result.
+    function _usub(uint256 x, uint256 y) internal pure returns (uint256 z) { z = x - y; }
+}
